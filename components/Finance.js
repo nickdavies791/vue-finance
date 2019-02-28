02a5007ef25334488e4cbc8ed7b2c1209e88fa38
Nick Davies <nickdavies07@outlook.com>
2019-02-28 10:25:42 +0000

    Add accounting end date for NBV calculation
    Adds the accounting end date to correctly calculate the net book value.
    Net book value will be the monthly depreciation x the months between the
    purchase date and the accounting end date.

--- components/Finance.js
+++ components/Finance.js
@@ -2,16 +2,16 @@ export default {
     template: `
         <div>
             <div class="form-group">
+                <label>Accounting End Date</label>
+                <input class="form-control" type="date" v-model="account_end">
+            </div>
+            <div class="form-group">
                 <label>Purchase Price</label>
                 <input class="form-control" type="text" v-model="cost">
             </div>
             <div class="form-group">
                 <label>Purchase Date</label>
                 <input class="form-control" type="date" v-model="start">
-            </div>
-            <div class="form-group">
-                <label>Transfer Date</label>
-                <input class="form-control" type="date" v-model="transfer">
             </div>
             <div class="form-group">
                 <label>End Date</label>
@@ -26,6 +26,18 @@ export default {
                 <input class="form-control" type="text" :value="getAnnualDepreciation" disabled>
             </div>
             <div class="form-group">
+                <label>Monthly Depreciation</label>
+                <input class="form-control" type="text" :value="getMonthlyDepreciation" disabled>
+            </div>
+            <div class="form-group">
+                <label>Net Book Value</label>
+                <input class="form-control" type="text" :value="getNetBookValue" disabled>
+            </div>
+            <div class="form-group">
+                <label>Transfer Date</label>
+                <input class="form-control" type="date" v-model="transfer">
+            </div>
+            <div class="form-group">
                 <label>Transferred Net Book Value</label>
                 <input class="form-control" type="text" :value="getTransferredNetBookValue" disabled>
             </div>
@@ -37,6 +49,7 @@ export default {
             cost: '5425.93',
             start: '2017-05-19',
             end: '2020-05-19',
+            account_end: '2017-08-31',
             transfer: '2017-08-19',
         }
     },
@@ -56,6 +69,28 @@ export default {
             return years;
         },
 
+        getTransferMonths() {
+            let start = moment(this.start);
+            let end = moment(this.transfer);
+            let months = end.diff(start, 'months');
+
+            return months;
+        },
+
+        getAccountingMonths() {
+            let start = moment(this.start);
+            let end = moment(this.account_end);
+            let months = end.diff(start, 'months');
+
+            return months;
+        },
+
+        getNetBookValue() {
+            let nbv = this.cost - this.getMonthlyDepreciation * this.getAccountingMonths;
+
+            return this.round(nbv);
+        },
+
         getPercentage() {
             let percentage = (1 / this.getYears);
 
@@ -65,16 +100,21 @@ export default {
         getAnnualDepreciation() {
             let cost = this.cost;
             let percentage = this.getPercentage;
-            let nbv = cost * percentage;
+            let annual_depreciation = cost * percentage;
 
-            return this.round(nbv);
+            return this.round(annual_depreciation);
+        },
+
+        getMonthlyDepreciation() {
+            let cost = this.cost;
+            let percentage = this.getPercentage;
+            let monthly_depreciation = cost * percentage / 12;
+
+            return monthly_depreciation;
         },
 
         getTransferredNetBookValue() {
-            let start = moment(this.start);
-            let end = moment(this.transfer);
-            let months = end.diff(start, 'months');
-            let nbv = this.cost - ((this.getAnnualDepreciation / 12) * months);
+            let nbv = this.cost - ((this.getAnnualDepreciation / 12) * this.getTransferMonths);
 
             return this.round(nbv);
         },
